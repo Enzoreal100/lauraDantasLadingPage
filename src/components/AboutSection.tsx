08d3b551fb7a9a558a425004ecea65f0880ee22e
Enzoreal100 <enzoreal100@gmail.com>
2025-07-15 20:36:55 -0300

    feat: added photos

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
-import aboutImage from '../assets/about.jpg'; // Import your image here
+import aboutImage from '../assets/about.jpg';
+import aboutVideo from '../assets/video-about.mp4';
+import aboutVideoCover from '../assets/video-about-cover.jpg';
+import { SectionTitle } from './ScheduleSection';
 
-const AboutSectionContainer = styled.section`
+const AboutSectionContainer = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+  box-sizing: border-box;
+`;
+const AboutSectionContent = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 40px 20px;
-  background-color: #f8f8f8; /* Example background color */
 
   @media (min-width: 768px) {
     flex-direction: row;
@@ -57,21 +66,50 @@ const AboutImage = styled.img`
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Example shadow */
 `;
 
+const VideoContainer = styled.div`
+  display: flex;
+  margin: 80px;
+  justify-content: center;
+  align-items: center;
+  height: 600px;
+  flex-direction: column;
+
+  @media (max-width: 768px) {
+    height: 300px;
+  }
+`;
+
 const AboutSection: React.FC = () => {
   return (
-    <AboutSectionContainer id='about'>
-      <TextContent>
-        <Title>Sobre</Title>
+    <AboutSectionContainer id="about">
+      <AboutSectionContent>
+        <TextContent>
+          <Title>Sobre</Title>
+          <Description>
+            Sou Laura Dantas, psicóloga com atuação baseada na Terapia Cognitivo-Comportamental (TCC), abordagem eficaz e comprovada cientificamente para a transformação de pensamentos, emoções e comportamentos.
+          </Description>
+          <Description>
+            Tenho pós-graduação em Psicopatologia, sou especialista em Transtorno Bipolar e possuo formação em Neurociência, o que amplia minha compreensão sobre os processos mentais e os desafios emocionais que impactam a vida cotidiana.
+          </Description>
+        </TextContent>
+        <ImageContainer>
+          <AboutImage src={aboutImage} alt="About Us" />
+        </ImageContainer>
+      </AboutSectionContent>
+      <VideoContainer>
+        <SectionTitle>
+          Conheça mais sobre meu trabalho
+        </SectionTitle>
         <Description>
-          Sou Laura Dantas, psicóloga com atuação baseada na Terapia Cognitivo-Comportamental (TCC), abordagem eficaz e comprovada cientificamente para a transformação de pensamentos, emoções e comportamentos.
+          Conheça mais sobre minha abordagem terapêutica e como posso ajudar você a superar desafios emocionais e alcançar uma vida mais equilibrada.
         </Description>
-        <Description>
-          Tenho pós-graduação em Psicopatologia, sou especialista em Transtorno Bipolar e possuo formação em Neurociência, o que amplia minha compreensão sobre os processos mentais e os desafios emocionais que impactam a vida cotidiana.
-        </Description>
-      </TextContent>
-      <ImageContainer>
-        <AboutImage src={aboutImage} alt="About Us" /> {/* Replace with your image source */}
-      </ImageContainer>
+        <video
+          src={aboutVideo}
+          controls
+          poster={aboutVideoCover}
+          style={{ width: 'auto', height: '100%', borderRadius: '8px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)', zIndex: 1000 }}
+        />
+      </VideoContainer>
     </AboutSectionContainer>
   );
 };
