7867f3b9a5ea7b3f69ecbfaa4ed3ccbe4f1256a3
Enzoreal100 <enzoreal100@gmail.com>
2025-07-18 19:28:22 -0300

    feat: add newsletter image and restructure NewsletterSection layout

--- src/components/NewsletterSection.tsx
+++ src/components/NewsletterSection.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import COLOR from '../constants/colors.constants';
 import CTAForms from './CTAForms';
+import newsletterImage from '../assets/newsletter.jpg';
 
 interface NewsletterSectionProps {
   title?: string;
@@ -11,15 +12,31 @@ interface NewsletterSectionProps {
 };
 
 export const NewsletterSectionContainer = styled.section`
+  display: flex;
   background-color: ${COLOR.BG.SECONDARY};
   text-align: center;
   scroll-margin-top: 80px;
-  padding: 80px;
+  padding: 40px 20px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 12rem;
 
   @media (max-width: 1024px) {
     padding: 40px 20px;
     margin-top: 80px;
+    flex-direction: column;
+    gap: 2rem;
   }
+`;
+
+const NewsletterSectionContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  max-width: 500px; 
+  
 `;
 
 export const SectionTitle = styled.h2`
@@ -27,8 +44,6 @@ export const SectionTitle = styled.h2`
   color: ${COLOR.TEXT.TITLE || '#333'};
   margin-bottom: 20px;
   max-width: 800px;
-  margin-left: auto;
-  margin-right: auto;
 `;
 
 export const SectionSubtitle = styled.p`
@@ -36,8 +51,6 @@ export const SectionSubtitle = styled.p`
   color: ${COLOR.TEXT.SUBTITLE || '#555'};
   margin-bottom: 40px; /* Mais espaço para o botão */
   max-width: 700px;
-  margin-left: auto;
-  margin-right: auto;
 `;
 
 export const StyledCTAButton = styled(CTAForms)`
@@ -46,17 +59,32 @@ export const StyledCTAButton = styled(CTAForms)`
   margin-bottom: 800px;
 `;
 
+const ImageContainer = styled.div`
+  max-width: 600px;
+`;
+
+const NewsletterImage = styled.img`
+  width: auto;
+  max-height: 400px; 
+  border-radius: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
 // Componente Principal da Seção de Newsletter
 const NewsletterSection: React.FC<NewsletterSectionProps > = ({ title, subtitle }) => {
   return (
-    <NewsletterSectionContainer>
-      <SectionTitle>{title}</SectionTitle>
-      <SectionSubtitle>
-        {subtitle}
-      </SectionSubtitle>
-      <StyledCTAButton />
-      
-    </NewsletterSectionContainer>
+      <NewsletterSectionContainer>
+        <ImageContainer>
+          <NewsletterImage src={newsletterImage} alt="Newsletter" />
+        </ImageContainer>
+        <NewsletterSectionContent>
+          <SectionTitle>{title}</SectionTitle>
+          <SectionSubtitle>
+            {subtitle}
+          </SectionSubtitle>
+          <StyledCTAButton />
+        </NewsletterSectionContent>
+      </NewsletterSectionContainer>
   );
 };
 
